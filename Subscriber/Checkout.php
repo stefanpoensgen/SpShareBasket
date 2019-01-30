5e03b03ad1721a823db26b073759d5657476178d
Stefan Poensgen <mail@stefanpoensgen.de>
2019-01-30 22:01:17 +0100

    :art: Moved config query to tpl

--- Subscriber/Checkout.php
+++ Subscriber/Checkout.php
@@ -4,7 +4,6 @@ namespace SpShareBasket\Subscriber;
 
 use Enlight\Event\SubscriberInterface;
 use Shopware\Components\DependencyInjection\Container as DIContainer;
-use Shopware\Components\Plugin\CachedConfigReader;
 
 class Checkout implements SubscriberInterface
 {
@@ -14,27 +13,13 @@ class Checkout implements SubscriberInterface
     private $container;
 
     /**
-     * @var string
-     */
-    private $pluginName;
-
-    /**
-     * @var CachedConfigReader
-     */
-    private $config;
-
-    /**
      * Checkout constructor.
      *
-     * @param DIContainer        $container
-     * @param string             $pluginName
-     * @param CachedConfigReader $config
+     * @param DIContainer $container
      */
-    public function __construct(DIContainer $container, string $pluginName, CachedConfigReader $config)
+    public function __construct(DIContainer $container)
     {
         $this->container = $container;
-        $this->pluginName = $pluginName;
-        $this->config = $config;
     }
 
     /**
@@ -49,8 +34,6 @@ class Checkout implements SubscriberInterface
 
     public function onPostDispatchCheckout(\Enlight_Event_EventArgs $args)
     {
-        $config = $this->config->getByPluginName($this->pluginName, $this->container->get('shop'));
-
         /** @var \Shopware_Controllers_Frontend_Checkout $subject */
         $subject = $args->getSubject();
         $request = $subject->Request();
@@ -61,17 +44,13 @@ class Checkout implements SubscriberInterface
 
             $router = $this->container->get('router');
             $sBasketUrl = $router->assemble([
-                'module' => 'frontend',
-                'sViewport' => 'sharebasket',
+                'controller' => 'sharebasket',
                 'action' => 'load',
                 'bID' => $basketID,
             ]);
 
             $view->assign('spShareBasket', true);
             $view->assign('sBasketUrl', $sBasketUrl);
-            $view->assign('spShareBasketEmail', $config['email']);
-            $view->assign('spShareBasketFacebook', $config['facebook']);
-            $view->assign('spShareBasketWhatsApp', $config['whatsapp']);
         }
     }
 }
